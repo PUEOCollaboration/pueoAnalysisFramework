177fc49e49aad077eaa21cc8d2a580a63e26f40c
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-11-17 11:03:51 -0600

    unported file

--- src/FilterOperation.cc
+++ src/FilterOperation.cc
@@ -1,23 +1,23 @@
-#include "FilterOperation.h" 
+#include "pueo/FilterOperation.h" 
 #include "TGraph.h" 
-#include "FilteredAnitaEvent.h" 
+#include "pueo/FilteredEvent.h" 
 #include <string.h> 
 #include <assert.h>
 
 
-FilterOperation::~FilterOperation()
+pueo::FilterOperation::~FilterOperation()
 {
 }
 
-ConditionalFilterOperation::~ConditionalFilterOperation()
+pueo::ConditionalFilterOperation::~ConditionalFilterOperation()
 {
  free (condition_tag); 
  free (condition_desc); 
  if (own) delete fo; 
 }
 
-ConditionalFilterOperation::ConditionalFilterOperation(UniformFilterOperation * operation, 
-                                                       bool (*condition)(FilteredAnitaEvent * ev, int ant, AnitaPol::AnitaPol_t), 
+pueo::ConditionalFilterOperation::ConditionalFilterOperation(UniformFilterOperation * operation, 
+                                                       bool (*condition)(FilteredEvent * ev, int ant, pol::pol_t), 
                                                        const char * condition_tag_suffix, const char * condition_description_suffix, bool should_own_operation) 
                                                        : fn(condition), fo(operation), own(should_own_operation)
 {
@@ -31,46 +31,46 @@ ConditionalFilterOperation::ConditionalFilterOperation(UniformFilterOperation * 
 
 
 
-AnalysisWaveform* FilterOperation::getWf(FilteredAnitaEvent *ev, int i) 
+pueo::AnalysisWaveform* pueo::FilterOperation::getWf(FilteredEvent *ev, int i) 
 { 
   return ev->filteredGraphs[i]; 
 }
 
-AnalysisWaveform* FilterOperation::getWf(FilteredAnitaEvent *ev, int ant, AnitaPol::AnitaPol_t pol) 
+pueo::AnalysisWaveform* pueo::FilterOperation::getWf(FilteredEvent *ev, int ant, pol::pol_t pol) 
 { 
   return ev->filteredGraphsByAntPol[pol][ant]; 
 }
 
 
-void ConditionalFilterOperation::process(FilteredAnitaEvent * ev) 
+void pueo::ConditionalFilterOperation::process(FilteredEvent * ev) 
 {
-  for (int pol = AnitaPol::kHorizontal; pol <= AnitaPol::kVertical; pol++)
+  for (int pol = pol::kHorizontal; pol <= pol::kVertical; pol++)
   {
 
 #ifdef USE_OMP
 #pragma omp parallel for
 #endif
-    for (int ant = 0; ant <NUM_SEAVEYS; ant++) 
+    for (int ant = 0; ant <k::NUM_HORNS; ant++) 
     {
-      if (fn(ev,ant, (AnitaPol::AnitaPol_t)pol))
+      if (fn(ev,ant, (pol::pol_t)pol))
       {
-        fo->processOne(getWf(ev,ant, (AnitaPol::AnitaPol_t)pol)); 
+        fo->processOne(getWf(ev,ant, (pol::pol_t)pol)); 
       }
     }
   }
 }
 
-void ConditionalFilterOperation::processOne(AnalysisWaveform* awf, const RawAnitaHeader* header, int ant, int pol) 
+void pueo::ConditionalFilterOperation::processOne(AnalysisWaveform* awf, const RawHeader* header, int ant, int pol) 
 {
 	printf("processOne not implemented yet, sorry!\n");
 }
 
-void UniformFilterOperation::process(FilteredAnitaEvent * ev) 
+void pueo::UniformFilterOperation::process(FilteredEvent * ev) 
 {
 #ifdef USE_OMP
 #pragma omp parallel for
 #endif
-  for (size_t i = 0; i < NUM_SEAVEYS * 2; i++) 
+  for (size_t i = 0; i < k::NUM_HORNS * 2; i++) 
   {
     processOne(getWf(ev,i)); 
   }
